9d27b57d42fa64899e47e77569fe5dca58fc33b7
Kylie Rose <thisiskylierose@gmail.com>
2018-12-13 21:21:43 +1000

    Added webpack to build process

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-browser-sync
+
+- https://medium.freecodecamp.org/how-to-use-reactjs-with-webpack-4-babel-7-and-material-design-ff754586f618
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from './index.css';
+
+const App = () => {
+  return <p>This works</p>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
