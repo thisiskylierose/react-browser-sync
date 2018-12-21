728ad08fce31846cf35cf17018d5cb23b4886215
Kylie Rose <kylie.rose@outlook.com>
2018-12-21 14:51:05 +1000

    Updated sample code to use flexbox example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import classNames from 'classnames';
 
 import styles from './index.css';
 
-const App = () => <div className={styles.wrapper}>This works!</div>;
+const App = () => (
+  <div className={styles.container}>
+    <div className={styles.content}>
+      <span className={styles.alert}>This works</span>
+    </div>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
